c9eb83c635c5a092a1f1720fd63c3d726e67c516
GoatLeywin <botcrae@gmail.com>
2023-11-02 14:50:19 -0500

    Update functions_cole.py

---
+++ Cole/functions_cole.py
@@ -0,0 +1,11 @@
+def menu():
+    #display menu accepts no arguments
+    #it outputs the menu choices and takes input from the user
+    # for the menu item and returns the choice
+    print('\t\tMENU')
+    print('1) Start new game')
+    print('2) Choose range')
+    print('3) Exit game')
+    
+    choice = int(input('Enter a selection: '))
+    return choice
