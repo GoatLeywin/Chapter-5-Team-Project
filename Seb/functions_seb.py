fe7cc7db2ed5a418853bd4855f86866789a650d1
Seb4354 <142840057+Seb4354@users.noreply.github.com>
2023-11-03 14:20:14 -0500

    Update functions_seb.py

---
+++ Seb/functions_seb.py
@@ -0,0 +1,15 @@
+def players():
+    #accepts no arguments
+    #prompts user for players first name and last name
+    #returns all variables
+    
+    #starting variable
+    player1f = input("Enter your first name: ")
+    player1l = input("Enter your last name: ")
+    player2f = input("Enter your first name: ")
+    player2l = input("Enter your last name: ")
+    
+    player1_name = player1f, player1l
+    print(player1_name)
+    player2_name = player2f, player2l
+    print(player2_name)
