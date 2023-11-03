095bac83bb1657e92e1312f0f3e23e44ef484296
GoatLeywin <botcrae@gmail.com>
2023-11-03 14:32:39 -0500

    Update function.py

--- Main/function.py
+++ Main/function.py
@@ -1,2 +1,21 @@
-def print():
-    print ('Hey')
+import random
+from random import randint
+
+def menu():
+    #display menu accepts no arguments
+    #it outputs the menu choices and takes input from the user
+    # for the menu item and returns the choice
+    print('\t\tMENU')
+    print('1) Start new game')
+    print('2) Choose range')
+    print('3) Exit game')
+    
+    choice = int(input('Enter a selection: '))
+    return choice
+
+def get_range(range_low, range_high):
+    #accepts range for the bottom and top number when the user decides the range
+    #will generate random number within range and return that number
+    random_number = randint(range_low, range_high)
+    
+    return random_number
